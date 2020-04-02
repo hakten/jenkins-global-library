4998e574c565b61a196ca0302b841e99164e8c98
Huseyin Akten <husakten@gmail.com>
2020-04-02 01:34:41 -0500

    label is changed to see if it works

--- src/com/lib/JenkinsCommonDockerBuildPipeline.groovy
+++ src/com/lib/JenkinsCommonDockerBuildPipeline.groovy
@@ -19,8 +19,60 @@
       .split('/')[0]
       .replace('-build', '-deploy')
 
-  podTemplate(name: k8slabel, label: k8slabel) {
-      node(k8slabel) {
+  def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: docker
+          image: docker:latest
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+            - mountPath: /etc/secrets/service-account/
+              name: google-service-account
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+            - mountPath: /etc/secrets/service-account/
+              name: google-service-account
+        serviceAccountName: common-service-account
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: google-service-account
+            secret:
+              secretName: google-service-account
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+  podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate) {
+      node(klabel) {
           stage("Pulling the code") {
             sh "echo ${repositoryName}"
             sh "echo ${deployJobName}"
