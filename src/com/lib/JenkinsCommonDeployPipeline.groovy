65e683e0c47cadfa53f5839b88b15dd531aa6b69
Huseyin Akten <husakten@gmail.com>
2020-04-02 16:53:04 -0500

    docker endpoint changed

--- src/com/lib/JenkinsCommonDeployPipeline.groovy
+++ src/com/lib/JenkinsCommonDeployPipeline.groovy
@@ -18,12 +18,12 @@ def runPipeline() {
     def versionList = []
     def token       = ""
     def myJsonreader = new JsonSlurper()
-    def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp"))
+    def nexusData = myJsonreader.parse(new URL("https://nexus.ggl.huseyinakten.net/service/rest/v1/components?repository=fuchicorp"))
     nexusData.items.each { if (it.name.contains(branchName)) { versionList.add(it.name + ":" + it.version) } }
     while (true) {
         if (nexusData.continuationToken) {
         token = nexusData.continuationToken
-        nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp&continuationToken=${token}"))
+        nexusData = myJsonreader.parse(new URL("https://nexus.ggl.huseyinakten.net/service/rest/v1/components?repository=fuchicorp&continuationToken=${token}"))
         nexusData.items.each { if (it.name.contains(branchName)) { versionList.add(it.name + ":" + it.version) } }
         }
         if (nexusData.continuationToken == null ) { break } }
