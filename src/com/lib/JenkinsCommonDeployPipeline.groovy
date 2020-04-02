a5ddb70ad0d917d768f52d16a381b51d5bae7139
Huseyin Akten <husakten@gmail.com>
2020-04-02 13:25:49 -0500

    changed the code from scratch

--- src/com/lib/JenkinsCommonDeployPipeline.groovy
+++ src/com/lib/JenkinsCommonDeployPipeline.groovy
@@ -1,46 +1,79 @@
+
 #!/usr/bin/env groovy
 package com.lib
 import groovy.json.JsonSlurper
+import static groovy.json.JsonOutput.*
 import hudson.FilePath
+
 
 
 def runPipeline() {
   def common_docker = new JenkinsDeployerPipeline()
-  def environment = ""
-  def gitCommitHash = ""
   def branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '').replace("/", "-").toLowerCase()
   def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
-  def repositoryName = "${JOB_NAME}"
-      .split('/')[0]
-      .replace('-fuchicorp', '')
-      .replace('-build', '')
-      .replace('-deploy', '')
-
-  def deployJobName = "${JOB_NAME}"
-      .split('/')[0]
-      .replace('-build', '-deploy')
-
-  if (branch.contains('dev-feature')) {
-    environment = 'dev' 
-    repositoryName = repositoryName + 'dev-feature'
-
-  } else if (branch.contains('qa-feature')) {
-    repositoryName = repositoryName + 'qa-feature'
-    environment = 'qa' 
-  }
-  
-  if (branch == 'master') {
-    environment = 'prod' 
-  } 
+  def allEnvironments = ['dev', 'qa', 'test', 'prod']
+  def findDockerImageScript = '''
+    import groovy.json.JsonSlurper
+    def findDockerImages(branchName) {
+    def versionList = []
+    def token       = ""
+    def myJsonreader = new JsonSlurper()
+    def nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp"))
+    nexusData.items.each { if (it.name.contains(branchName)) { versionList.add(it.name + ":" + it.version) } }
+    while (true) {
+        if (nexusData.continuationToken) {
+        token = nexusData.continuationToken
+        nexusData = myJsonreader.parse(new URL("https://nexus.fuchicorp.com/service/rest/v1/components?repository=fuchicorp&continuationToken=${token}"))
+        nexusData.items.each { if (it.name.contains(branchName)) { versionList.add(it.name + ":" + it.version) } }
+        }
+        if (nexusData.continuationToken == null ) { break } }
+    if(!versionList) { versionList.add("ImmageNotFound") } 
+    return versionList.reverse(true) }
+    findDockerImages('%s')
+  '''
+  def deploymentName = "${JOB_NAME}".split('/')[0].replace('-fuchicorp', '').replace('-build', '').replace('-deploy', '')
 
   try {
-    properties([
-      parameters([
-        booleanParam(defaultValue: false,
-          description: 'Click this if you would like to deploy to latest',
-          name: 'PUSH_LATEST'
-          )])])
+    // Trying to build the job
+    properties([ parameters([
 
+      // Boolean Paramater for terraform apply or not 
+      booleanParam(defaultValue: false, 
+      description: 'Apply All Changes', 
+      name: 'terraform_apply'),
+
+      // Boolean Paramater for terraform destroy 
+      booleanParam(defaultValue: false, 
+      description: 'Destroy deployment', 
+      name: 'terraform_destroy'),
+
+      // ExtendedChoice Script is getting all jobs based on this application
+      extendedChoice(bindings: '', description: 'Please select docker image to deploy', 
+      descriptionPropertyValue: '', groovyClasspath: '', 
+      groovyScript:  String.format(findDockerImageScript, deploymentName) , multiSelectDelimiter: ',', 
+      name: 'selectedDockerImage', quoteValue: false, 
+      saveJSONParameterToFile: false, type: 'PT_SINGLE_SELECT', 
+      visibleItemCount: 5),
+      
+      // list of environment getting from <allEnvironments> and defining variable <environment> to deploy 
+      choice(name: 'environment', 
+      choices: allEnvironments, 
+      description: 'Please select the environment to deploy'),
+
+      // Extra configurations to deploy with it 
+      text(name: 'deployment_tfvars', 
+      defaultValue: 'extra_values = "tools"', 
+      description: 'terraform configuration'),
+
+      // Boolean Paramater for debuging this job 
+      booleanParam(defaultValue: false, 
+      description: 'If you would like to turn on debuging click this!!', 
+      name: 'debugMode')
+
+      ]
+      )])
+
+      // Jenkins slave to build this job 
       def slavePodTemplate = """
       metadata:
         labels:
@@ -94,67 +127,137 @@ def runPipeline() {
               path: /var/run/docker.sock
     """
 
-  podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate) {
+  podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: params.debugMode) {
       node(k8slabel) {
 
+        stage("Deployment Info") {
+
+          // Colecting information to show on stage <Deployment Info>
+          println(prettyPrint(toJson([
+            "Environment" : environment,
+            "Deployment" : deploymentName,
+            "Builder" : getBuildUser(),
+            "Build": env.BUILD_NUMBER
+          ])))
+        }
+
         container('fuchicorptools') {
-          stage("Pulling the code") {
+
+          stage("Polling SCM") {
             checkout scm
-            gitCommitHash = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
           }
 
-          stage('Build docker image') {
-            dir("${WORKSPACE}/deployments/docker") {
-              // Build the docker image
-              dockerImage = docker.build(repositoryName, "--build-arg branch_name=${branch} .")
-            }
-          }
+          stage('Generate Configurations') {
+            sh """
+              mkdir -p ${WORKSPACE}/deployments/terraform/
+              cat  /etc/secrets/service-account/credentials.json > ${WORKSPACE}/deployments/terraform/fuchicorp-service-account.json
+              ls ${WORKSPACE}/deployments/terraform/
+              ## This script should move to docker container to set up ~/.kube/config
+              sh /scripts/Dockerfile/set-config.sh
+            """
+            // sh /scripts/Dockerfile/set-config.sh Should go to Docker container CMD so we do not have to call on slave 
+            deployment_tfvars += """
+              deployment_name        = \"${deploymentName}\"
+              deployment_environment = \"${environment}\"
+              deployment_image       = \"docker.fuchicorp.com/${selectedDockerImage}\"
+              credentials            = \"./fuchicorp-service-account.json\"
+            """.stripIndent()
 
-          stage('Push image') {
+            writeFile(
+              [file: "${WORKSPACE}/deployments/terraform/deployment_configuration.tfvars", text: "${deployment_tfvars}"]
+              )
 
-
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "nexus-docker-creds", usernameVariable: 'docker_username', passwordVariable: 'docker_password']]) {
-              sh """#!/bin/bash -e
-
-              until docker login --username ${env.docker_username} --password ${env.docker_password} https://docker.ggl.huseyinakten.net
-              do
-                echo "Trying to login to docker private system"
-                sleep 3
-              done
+            if (params.debugMode) {
+              sh """
+                echo #############################################################
+                cat ${WORKSPACE}/deployments/terraform/deployment_configuration.tfvars
+                echo #############################################################
               """
             }
+            
+            try {
+                withCredentials([
+                    file(credentialsId: "${deploymentName}-config", variable: 'default_config')
+                ]) {
+                    sh """
+                      #!/bin/bash
+                      cat \$default_config >> ${WORKSPACE}/deployments/terraform/deployment_configuration.tfvars
+                      
+                    """
+                    if (params.debugMode) {
+                      sh """
+                        echo #############################################################
+                        cat ${WORKSPACE}/deployments/terraform/deployment_configuration.tfvars
+                        echo #############################################################
+                      """
+                    }
+                }
+            
+                println("Found default configurations appanded to main configuration")
+            } catch (e) {
+                println("Default configurations not founds. Skiping!!")
+            }
+              
+          }
+          stage('Terraform Apply/Plan') {
+            if (!params.terraform_destroy) {
+              if (params.terraform_apply) {
 
+                dir("${WORKSPACE}/deployments/terraform/") {
+                  echo "##### Terraform Applying the Changes ####"
+                  sh '''#!/bin/bash -e
+                    source set-env.sh deployment_configuration.tfvars
+                    terraform apply --auto-approve -var-file=deployment_configuration.tfvars
+                  '''
+                }
 
-             // Push image to the Nexus with new release
-              docker.withRegistry('https://docker.ggl.huseyinakten.net', 'nexus-docker-creds') {
-                  dockerImage.push("${gitCommitHash}") 
+              } else {
 
-                  if (params.PUSH_LATEST) {
-                    dockerImage.push("latest")
+                dir("${WORKSPACE}/deployments/terraform/") {
+                  echo "##### Terraform Plan (Check) the Changes #### "
+                  sh '''#!/bin/bash -e
+                    source set-env.sh deployment_configuration.tfvars
+                    terraform plan -var-file=deployment_configuration.tfvars
+                  '''
                 }
               }
+            }
+          }
 
+          stage('Terraform Destroy') {
+            if (!params.terraform_apply) {
+              if (params.terraform_destroy) {
+                if ( environment != 'tools' ) {
+                  dir("${WORKSPACE}/deployments/terraform/") {
+                    echo "##### Terraform Destroing ####"
+                    sh '''#!/bin/bash -e
+                      source set-env.sh deployment_configuration.tfvars
+                      terraform destroy --auto-approve -var-file=deployment_configuration.tfvars
+                    '''
+                  }
+                } else {
+                  println("""
 
-           }
+                    Sorry I can not destroy Tools!!!
+                    I can Destroy only dev and qa branch
 
-           stage("Clean up") {
-              sh "docker rmi --no-prune docker.ggl.huseyinakten.net/${repositoryName}:${gitCommitHash}"
-
-              if (params.PUSH_LATEST) {
-                sh "docker rmi --no-prune docker.ggl.huseyinakten.net/${repositoryName}:latest"
+                  """)
+                }
               }
            }
 
-           stage("Trigger Deploy") {
-              build job: "${deployJobName}/master", 
-              parameters: [
-                  [$class: 'BooleanParameterValue', name: 'terraform_apply', value: true],
-                  [$class: 'StringParameterValue', name: 'selectedDockerImage', value: "${repositoryName}:${gitCommitHash}"], 
-                  [$class: 'StringParameterValue', name: 'environment', value: "${environment}"]
-                  ]
-           }
+           if (params.terraform_destroy) {
+             if (params.terraform_apply) {
+               println("""
 
-         }
+                Sorry you can not destroy and apply at the same time
+
+               """)
+               currentBuild.result = 'FAILURE'
+            }
+          }
+        }
+       }
       }
     }
   } catch (e) {
@@ -164,6 +267,15 @@ def runPipeline() {
   }
 }
 
-
+// Function to get user id 
+@NonCPS
+def getBuildUser() {
+      try {
+        return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
+      } catch (e) {
+        def user = "AutoTrigger"
+        return user
+      }
+  }
 
 return this
