c03f0b32b7de6587c01b74236d6b308f4df55aa0
Huseyin Akten <husakten@gmail.com>
2020-04-02 14:12:40 -0500

    get users removed

--- src/com/lib/JenkinsCommonDeployPipeline.groovy
+++ src/com/lib/JenkinsCommonDeployPipeline.groovy
@@ -267,15 +267,15 @@ def runPipeline() {
   }
 }
 
-// Function to get user id 
-@NonCPS
-def getBuildUser() {
-      try {
-        return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
-      } catch (e) {
-        def user = "AutoTrigger"
-        return user
-      }
-  }
+// // Function to get user id 
+// @NonCPS
+// def getBuildUser() {
+//       try {
+//         return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
+//       } catch (e) {
+//         def user = "AutoTrigger"
+//         return user
+//       }
+//   }
 
-return this
+// return this
